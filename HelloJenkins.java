76adba3a4ee6a2810823611eb8d8815d41a1ff4b
Dharsini L <68725945+dharsinil@users.noreply.github.com>
2024-09-17 21:21:59 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins{
   public static void main(String[] args){
-    System.out.println("****.....Trying Webhook1....****");
+    System.out.println("****.....Trying Webhook2....****");
 }
 }
